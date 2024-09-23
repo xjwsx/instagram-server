82db2bac703686f1b1a62fa885d5b23f5c5bd5c8
xjwsx <wlstmdghs@naver.com>
2024-09-23 17:47:26 +0900

    feat: query parameters

--- index.js
+++ index.js
@@ -6,8 +6,45 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
+const mockUsers = [
+  { id: 1, username: "anson", displayName: "Anson" },
+  { id: 2, username: "jack", displayName: "Jack" },
+  { id: 3, username: "adam", displayName: "Adam" },
+  { id: 4, username: "tina", displayName: "Tina" },
+  { id: 5, username: "jason", displayName: "Jason" },
+  { id: 6, username: "henry", displayName: "Henry" },
+  { id: 7, username: "marilyn", displayName: "Marilyn" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/api/users", (req, res) => {
+  console.log(req.query);
+  const {
+    query: { filter, value },
+  } = req;
+  if (!filter && !value) return res.send(mockUsers);
+
+  if (filter && value)
+    return res.send(mockUsers.filter((user) => user[filter].includes(value)));
+});
+
+app.get("/api/users/:id", (req, res) => {
+  console.log(req.params);
+  const parsedId = parseInt(req.params.id);
+  console.log(parsedId);
+  if (isNaN(parsedId))
+    return res.status(400).send({ msg: "Bad Request. Invalid Id." });
+
+  const findUser = mockUsers.find((user) => user.id === parsedId);
+  if (!findUser) return res.sendStatus(404);
+  return res.send(findUser);
+});
+
+app.get("/api/products", (req, res) => {
+  res.send([{ id: 123, name: "chicken breast", price: 12.99 }]);
 });
 
 app.listen(PORT, () => {
